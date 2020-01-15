61cb2ca20b0cab136cc518ebdbbeb4f674f22e9a
ios2 <5891324123@qq.com>
2020-01-15 18:13:15 +0800

    hooks in

--- components/layout.jsx
+++ components/layout.jsx
@@ -7,7 +7,7 @@ export default ({ children }) => (
             <Link href="/a?id=1" as="/a/1">
                 <Button>jump A</Button>
             </Link>
-            <Link href="test/b">
+            <Link href="/test/b">
                 <Button>jump B</Button>
             </Link>
         </header>
--- pages/test/b.js
+++ pages/test/b.js
@@ -1,7 +1,59 @@
+import React, { useState, useEffect, useReducer } from "react";
 import Component from "../../components/component";
-import { withRouter } from "next/router";
 
-const B = ({ router }) => <Component>B--{router.query.id}</Component>
+class B extends React.Component {
+    state = {
+        count: 0
+    }
 
+    componentDidMount() {
+        this.timer = setInterval(() => {
+            this.setState({
+                count: ++this.state.count
+            })
+        }, 1000)
+    }
 
-export default withRouter(B);
+    componentWillUnmount() {
+        if (this.timer) {
+            clearInterval(this.timer);
+        }
+    }
+
+    render() {
+        return <span>{this.state.count}</span>
+    }
+}
+
+// Hooks
+function BFunc() {
+    // const [count, setCount] = useState(0); // 返回[]，结构赋值
+    const [name, setName] = useState("zya");
+
+    const [count, dispatchCount] = useReducer(BReducer, 0); // 复杂变量使用reducer（对象）
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            // setCount(25);
+            // setCount(c => ++c);
+            dispatchCount({ type: "add" });
+        }, 1000)
+        return () => clearInterval(timer);
+    }, [])
+
+    return <span>{count}</span>
+}
+
+function BReducer(state, action) {
+    switch (action.type) {
+        case "add":
+            return state + 1
+        case "minus":
+            return state - 1
+        default:
+            return state
+    }
+}
+
+// export default B;
+export default BFunc;
