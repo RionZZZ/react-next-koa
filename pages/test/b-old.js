c2e0540810ccae157e5505e34bca4be6f9bf764b
Zhengyuang <rion.z924@gmail.com>
2020-01-16 17:23:04 +0800

    memo hooks

---
+++ pages/test/b-old.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect, useReducer, useLayoutEffect, useContext, useRef } from "react";
+import Component from "../../components/component";
+
+import MyContext from "../../lib/my-context";
+
+class B extends React.Component {
+    state = {
+        count: 0
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(() => {
+            this.setState({
+                count: ++this.state.count
+            })
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        if (this.timer) {
+            clearInterval(this.timer);
+        }
+    }
+
+    render() {
+        return <span>{this.state.count}</span>
+    }
+}
+
+// Hooks
+function BFunc() {
+    // const [count, setCount] = useState(0); // 返回[]，结构赋值
+    const [name, setName] = useState("zya");
+
+    const [count, dispatchCount] = useReducer(BReducer, 0); // 复杂变量使用reducer（对象）
+
+    const context = useContext(MyContext);
+
+    const inputRef = useRef();
+
+    // useEffect(() => {
+    //     const timer = setInterval(() => {
+    //         // setCount(25);
+    //         // setCount(c => ++c);
+    //         dispatchCount({ type: "add" });
+    //     }, 1000)
+    //     return () => clearInterval(timer);
+    // }, [])
+
+    useEffect(() => {
+        console.log("effect invoke");
+        console.log(inputRef);
+        return () => {
+            console.log("effect detect");
+        }
+    }, [name])
+
+    // 比effect先执行，在dom挂载到html上去之前执行。 会卡，少用..
+    // useLayoutEffect(() => {
+    //     console.log("useLayoutEffect invoke");
+    //     return () => {
+    //         console.log("useLayoutEffect detect");
+    //     }
+    // }, [name])
+
+    return (
+        <div>
+            <span>{count}</span>
+            <span>{name}</span>
+            <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />
+            <button onClick={() => dispatchCount({ type: "add" })}>{count}</button>
+            <p>{context}</p>
+        </div>
+    )
+}
+
+function BReducer(state, action) {
+    switch (action.type) {
+        case "add":
+            return state + 1
+        case "minus":
+            return state - 1
+        default:
+            return state
+    }
+}
+
+// export default B;
+export default BFunc;
--- pages/test/b.js
+++ pages/test/b.js
@@ -1,72 +1,34 @@
-import React, { useState, useEffect, useReducer, useLayoutEffect, useContext } from "react";
-import Component from "../../components/component";
+import React, { useState, useReducer, memo, useMemo, useCallback, useRef } from "react";
 
-import MyContext from "../../lib/my-context";
-
-class B extends React.Component {
-    state = {
-        count: 0
-    }
-
-    componentDidMount() {
-        this.timer = setInterval(() => {
-            this.setState({
-                count: ++this.state.count
-            })
-        }, 1000)
-    }
-
-    componentWillUnmount() {
-        if (this.timer) {
-            clearInterval(this.timer);
-        }
-    }
-
-    render() {
-        return <span>{this.state.count}</span>
-    }
-}
-
-// Hooks
 function BFunc() {
-    // const [count, setCount] = useState(0); // 返回[]，结构赋值
-    const [name, setName] = useState("zya");
-
+    const [name, setName] = useState("rion");
     const [count, dispatchCount] = useReducer(BReducer, 0); // 复杂变量使用reducer（对象）
 
-    const context = useContext(MyContext);
+    const config = useMemo(() => ({
+        text: `count is ${count}`,
+        color: count > 3 ? "red" : "blue"
+    }), [count])
 
-    // useEffect(() => {
-    //     const timer = setInterval(() => {
-    //         // setCount(25);
-    //         // setCount(c => ++c);
-    //         dispatchCount({ type: "add" });
-    //     }, 1000)
-    //     return () => clearInterval(timer);
-    // }, [])
+    const handleButtonClick = useCallback(() => dispatchCount({ type: "add" }), [count]);
+    // const handleButtonClick = useMemo(() => () => dispatchCount({ type: "add" }), []);
 
-    useEffect(() => {
-        console.log("effect invoke");
-        return () => {
-            console.log("effect detect");
-        }
-    }, [name])
 
-    // 比effect先执行，在dom挂载到html上去之前执行。 会卡，少用..
-    useLayoutEffect(() => {
-        console.log("useLayoutEffect invoke");
-        return () => {
-            console.log("useLayoutEffect detect");
-        }
-    }, [name])
+    const countRef = useRef(); // {current:''} useRef每次返回同一个对象，不会创建新对象
+    countRef.current = count;
+    const handleAlert = () => {
+        setTimeout(() => {
+            // alert(count); // 闭包
+            alert(countRef.current);
+        }, 1000);
+    }
 
     return (
         <div>
-            <span>{count}</span>
-            <span>{name}</span>
             <input value={name} onChange={e => setName(e.target.value)} />
-            <button onClick={() => dispatchCount({ type: "add" })}>{count}</button>
-            <p>{context}</p>
+            <Child
+                config={config}
+                onButtonClick={handleButtonClick} />
+            <button onClick={handleAlert}>alert count</button>
         </div>
     )
 }
@@ -82,5 +44,14 @@ function BReducer(state, action) {
     }
 }
 
-// export default B;
+// function Child({ onButtonClick, config }) {
+const Child = memo(function Child({ onButtonClick, config }) {
+    console.log("child render");
+    return (
+        <button onClick={onButtonClick} style={{ color: config.color }}>
+            {config.text}
+        </button>
+    )
+})
+
 export default BFunc;
